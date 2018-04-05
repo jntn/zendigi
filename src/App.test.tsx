5ccd58035d67d04d29cdd8895b9e3d611a26ab45
Jonatan Granqvist <jonatan.granqvist@dotnetmentor.se>
2018-04-05 22:45:01 +0200

    Updated test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,17 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { Provider } from 'mobx-react'
+import App from './App'
+import TimelineStore from './stores/TimelineStore'
+
+const timelineStore = TimelineStore.create()
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <Provider timelineStore={timelineStore}>
+      <App />
+    </Provider>,
+    div
+  )
+})
